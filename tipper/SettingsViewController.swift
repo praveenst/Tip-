f941934768a4b979489b29a569ec04f750088d72
praveenst <praveenst@gmail.com>
2017-03-06 06:54:04 -0800

    tip calculator version 1.0
    The following **required** functionality is complete:
    
    * [x] User can enter a bill amount, choose a tip percentage, and see the tip and total values.
    * [x] Settings page to change the default tip percentage.
    
    The following **optional** features are implemented:
    * [x] Remembering the bill amount across app restarts 
    * [x] Making sure the keyboard is always visible and the bill amount is always the first responder. This way the user doesn't have to tap anywhere to use this app. Just launch the app and start typing.

---
+++ tipper/SettingsViewController.swift
@@ -0,0 +1,71 @@
+//
+//  SettingsViewController.swift
+//  tipper
+//
+//  Created by Praveen Thangavelu on 3/4/17.
+//  Copyright © 2017 Praveen Thangavelu. All rights reserved.
+//
+
+import UIKit
+
+
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var tipSegmentControl: UISegmentedControl!
+    
+    let userDefaults = UserDefaults.standard
+    
+    @IBAction func setDefaultTip(_ sender: Any) {
+        
+        
+        switch (tipSegmentControl.selectedSegmentIndex) {
+            
+        case 0:
+            self.view.backgroundColor = UIColor.lightGray
+        case 1:
+            self.view.backgroundColor = UIColor.gray
+        case 2:
+            self.view.backgroundColor = UIColor.darkGray
+        default:
+            self.view.backgroundColor = UIColor.clear
+        }
+        
+        let bgColor = tipSegmentControl.selectedSegmentIndex
+        userDefaults.set(bgColor, forKey: "bg-color")
+        userDefaults.synchronize()
+        
+    }
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let bgColVal = userDefaults.integer(forKey: "bg-color")
+        tipSegmentControl.selectedSegmentIndex = bgColVal
+        
+        
+        switch (bgColVal) {
+            
+        case 0:
+            self.view.backgroundColor = UIColor.lightGray
+        case 1:
+            self.view.backgroundColor = UIColor.gray
+        case 2:
+            self.view.backgroundColor = UIColor.darkGray
+        default:
+            self.view.backgroundColor = UIColor.clear
+        }
+        
+
+    }
+  
+   // Do any additional setup after loading the view.
+  override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
---
+++ tipper/tipper/ViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  tipper
+//
+//  Created by Praveen Thangavelu on 3/4/17.
+//  Copyright © 2017 Praveen Thangavelu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let userDefaults = UserDefaults.standard
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var totalDollarLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipPercentControl: UISegmentedControl!
+    @IBOutlet weak var partyLabel: UILabel!
+    @IBOutlet weak var partySizeStepper: UIStepper!
+    @IBOutlet weak var perPersonTipLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        getUserDefaults()
+        billField.becomeFirstResponder()
+        
+
+    }
+    
+    private func getUserDefaults() {
+        
+        let bgColVal = userDefaults.integer(forKey: "bg-color")
+        tipPercentControl.selectedSegmentIndex = bgColVal
+        
+        print(bgColVal)
+        
+        switch (bgColVal) {
+        case 0:
+            self.view.backgroundColor = UIColor.lightGray
+        case 1:
+            self.view.backgroundColor = UIColor.gray
+        case 2:
+            self.view.backgroundColor = UIColor.darkGray
+        default:
+            self.view.backgroundColor = UIColor.clear
+        }
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getUserDefaults()
+        billField.becomeFirstResponder()
+        print("view will appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+  
+
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true);
+        
+        //print("Hello");
+    }
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.20, 0.25];
+        
+        let bill = Double(billField.text!) ?? 0;
+        let tip = bill * tipPercentages[tipPercentControl.selectedSegmentIndex];
+        let total = bill + tip;
+        let size = partySizeStepper.value;
+        let personValue = total / size;
+        
+        //format can also be done this way
+        tipLabel.text = String(format: "$%.2f", tip);
+        // to get 2 decimal places for %f, make it .2f
+        totalDollarLabel.text = String(format: "$%.2f", total);
+        partyLabel.text = String(format: "%.0f", size);
+        perPersonTipLabel.text = String(format: "$ %.2f", personValue);
+        
+        
+    }
+}
+
